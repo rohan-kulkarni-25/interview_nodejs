dd0bc5bb628e8d419667813f3a3ee0b4aa829ddd
helewrer3 <helewrer3@gmail.com>
2021-01-17 10:18:51 +0530

    Mongo Works

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const async = require('async');
 const app = express();
 app.use(express.json());
+app.set('view engine', 'hbs');
 app.use(express.urlencoded({extended: true}));
-mongoose.connect("<Your URI here>", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("<Your URI>", { useNewUrlParser: true, useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 const sch_data = new Schema({
     name: String,
@@ -12,18 +14,15 @@ const sch_data = new Schema({
 });
 const md_data = mongoose.model('interview_model', sch_data);
 
-function check(interviews, data){
-    var f = 0;
-    f |= (interviews.id.length<2);
-    for(let i = 0; i < interviews.id.length; i++)f |= isNaN(+interviews.id[i]);
-    if(!f)for(let i = 0; i < interviews.id.length; i++)f |= ((interviews.id[i]-1) >= data.length || (interviews.id[i]-1) < 0);
-    if(!f)for(let i = 0; i < interviews.id.length; i++)f |= (data[(interviews.id[i]-1)].st != -1);
-    return f;
-}
-
 app.get('/', function (req, res) {res.status(200).sendFile(__dirname + '/index.html');})
 
-app.get('/new', function (req, res) {res.status(200).sendFile(__dirname + '/form.html');})
+app.get('/new', function (req, res) {
+    const val = md_data.find({}, function (err, data) {
+        if(err) throw err;
+        res.status(200).render('form', {names: data});
+        return data;
+    });
+})
 
 app.get('/list', function (req, res) {
     const val = md_data.find({}, function (err, data) {
@@ -34,22 +33,30 @@ app.get('/list', function (req, res) {
 })
 
 app.post('/new/add', function (req, res) {
-    var interviews = req.body;
-    md_data.find({}, function (err, data) {
-        if(err) throw err;
-        interviews.id = interviews.id.split(',');
-        if(check(interviews, data))res.status(404).send('Invalid input.');
-        else{
-            for(let i = 0; i < interviews.id.length; i++){
-                md_data.findOneAndUpdate({name: interviews.id[i]}, {st: interviews.st, ed: interviews.ed}, function(err, data){
-                    if(err) throw err;
-                    return data;
-                })
+    var interviews = req.body, f = 0;
+    if(typeof interviews.id == 'string') res.status(404).send('Wrong Input');
+    else{
+        async.eachSeries(interviews.id, function(obj, callback){
+            md_data.findById(obj, function(err, data){
+                if(err) throw err;
+                var val = data.toObject();
+                if(val.st != -1)f = 1;
+                callback();
+            })
+        }, function (err) {
+            if(err)throw err;
+            if(f) res.status(404).send('Wrong Input');
+            else{
+                for(let i = 0; i < interviews.id.length; i++){
+                    md_data.findByIdAndUpdate(interviews.id[i], {st: interviews.st, ed: interviews.ed}, function(err, data){
+                        if(err) throw err;
+                        return data;
+                    })
+                }
+                res.redirect('/')
             }
-            res.redirect('/');
-        }
-        return data;
-    });
+        })
+    }
 })
 
 const port = process.env.POR || 3000;
